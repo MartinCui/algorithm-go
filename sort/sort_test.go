3a0ece5f62067782ce188a64031ef821f92b5263
Martin Cui <martin.cui@ef.com>
2020-05-03 12:09:24 +0100

    binary heap data structure implementation

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -2,6 +2,7 @@ package sort
 
 import (
 	"github.com/stretchr/testify/require"
+	"math"
 	"math/rand"
 	"sort"
 	"testing"
@@ -26,23 +27,55 @@ func TestInsertion(t *testing.T) {
 	testSort(t, insertionSort)
 }
 
-func TestShell(t *testing.T){
+func TestShell(t *testing.T) {
 	testSort(t, shellSort)
 }
 
-func TestMerge(t *testing.T){
+func TestMerge(t *testing.T) {
 	testSort(t, mergeSort)
 }
 
-func TestQuick(t *testing.T){
+func TestQuick(t *testing.T) {
 	testSort(t, quickSort)
 }
 
-func makeRandomSlice(size int) []int {
+func TestBinaryHeap(t *testing.T) {
+	rq := require.New(t)
+	heap := newHeapPriorityQueue()
+	randomSlice := makeRandomSlice(10, true)
+	for _, v := range randomSlice {
+		heap.add(v)
+	}
+	randomSlice2 := makeRandomSlice(10, true)
+	for i := 0; i < len(randomSlice)/2; i++ {
+		heap.popMax()
+		heap.add(randomSlice2[i])
+		heap.add(randomSlice2[i] + len(randomSlice)/2)
+	}
+
+	previousMax := math.MaxInt32
+	for count := 0; count < int(float32(len(randomSlice))*1.5); count++ {
+		currentMax := heap.popMax()
+		rq.GreaterOrEqual(previousMax, currentMax)
+		previousMax = currentMax
+	}
+
+	rq.Equal(0, heap.size)
+}
+
+func makeRandomSliceNoLimit(size int) []int {
+	return makeRandomSlice(size, false)
+}
+
+func makeRandomSlice(size int, limitMax bool) []int {
 	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 	arr := make([]int, size)
 	for i := 0; i < len(arr); i++ {
-		arr[i] = rnd.Int()
+		if limitMax {
+			arr[i] = rnd.Intn(100)
+		} else {
+			arr[i] = rnd.Int()
+		}
 	}
 	return arr
 }
@@ -50,7 +83,7 @@ func makeRandomSlice(size int) []int {
 func testSort(t *testing.T, sortFun func([]int)) {
 	rq := require.New(t)
 	for i := 0; i < testStoreTimes; i++ {
-		arr := makeRandomSlice(testArraySize)
+		arr := makeRandomSliceNoLimit(testArraySize)
 
 		correctResult := make([]int, len(arr))
 		copy(correctResult, arr)
@@ -64,7 +97,7 @@ func testSort(t *testing.T, sortFun func([]int)) {
 func benchmarkSort(b *testing.B, sortFun func([]int)) {
 	randomArrays := make([][]int, b.N)
 	for i := 0; i < len(randomArrays); i++ {
-		randomArrays[i] = makeRandomSlice(benchArraySize)
+		randomArrays[i] = makeRandomSliceNoLimit(benchArraySize)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -95,4 +128,3 @@ func BenchmarkMerge(b *testing.B) {
 func BenchmarkQuick(b *testing.B) {
 	benchmarkSort(b, quickSort)
 }
-
